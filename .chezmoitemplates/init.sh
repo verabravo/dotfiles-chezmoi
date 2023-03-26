80966e353571a096da81d741d4bf5cc8aceb8ff4
bytaj <theantoniojose97@gmail.com>
2023-03-26 01:15:18 +0100

    encrypted files added

--- .chezmoitemplates/init.sh
+++ .chezmoitemplates/init.sh
@@ -3,3 +3,4 @@
 source "$(chezmoi source-path)/custom_files/shell/exports.sh"
 source "$(chezmoi source-path)/custom_files/shell/functions.sh"
 source "$HOME/bin/fzf-docker/docker-fzf"
+source "$HOME/.extra_bash_function.sh"
